b8d3bebad6543fef70eee01741d5417edd4e2b76
Abhishek Yadav <abhishek101242144@gmail.com>
2026-05-07 15:10:27 +0530

    fix: DS-04 add Sonner Toaster to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { QueryProvider } from "@/components/providers/query-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AuthProvider } from "@/components/providers/auth-provider";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const inter = Inter({
@@ -64,6 +65,7 @@ export default function RootLayout({
             </TooltipProvider>
           </ThemeProvider>
         </QueryProvider>
+        <Toaster richColors position="bottom-right" />
       </body>
     </html>
   );
